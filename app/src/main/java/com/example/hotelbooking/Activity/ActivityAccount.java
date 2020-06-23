e8df54968fbf282eedcabe62780d63c5238ea49c
DESKTOP-QG8M2S6\Admin <huongcoolnguyen192@gmail.com>
2020-06-24 02:42:28 +0700

    Lấy được thông tin của tài khoản đăng nhập facebook, chức năng log out trong activity Account đang lỗi.

--- app/src/main/java/com/example/hotelbooking/Activity/ActivityAccount.java
+++ app/src/main/java/com/example/hotelbooking/Activity/ActivityAccount.java
@@ -2,35 +2,50 @@ package com.example.hotelbooking.Activity;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.example.hotelbooking.R;
 import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class ActivityAccount extends AppCompatActivity {
-    ImageView avatar;
-    TextView txtName, txtEmail;
+    private ImageView avatar;
+    private TextView txtName, txtEmail;
+    private Button btnBack, btnChangeInfor, btnBookingHistory, btnFavouriteList, btnLogout;
+    FirebaseAuth maAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_new);
-
-
+        maAuth.getInstance();
         txtName = findViewById(R.id.txtUsername);
         avatar = findViewById(R.id.imageView);
         txtEmail = findViewById(R.id.txtEmail);
+        btnBack = findViewById(R.id.btnBack);
+        btnChangeInfor = findViewById(R.id.btnChangeInfor);
+        btnBookingHistory = findViewById(R.id.btnBookingHistory);
+        btnFavouriteList = findViewById(R.id.btnFaList);
+        btnLogout = findViewById(R.id.btnLogout);
+
         boolean logout = AccessToken.getCurrentAccessToken() == null;
         if(!logout){
             Picasso.with(this).load(Profile.getCurrentProfile().
@@ -38,9 +53,29 @@ public class ActivityAccount extends AppCompatActivity {
             Log.d("TAG", "Username:" + Profile.getCurrentProfile().getName());
             getUserProfile(AccessToken.getCurrentAccessToken());
         }
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityAccount.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                maAuth.signOut();
+                LoginManager.getInstance().logOut();
+                updateUI();
+            }
+        });
     }
+    private void updateUI() {
+        Toast.makeText(ActivityAccount.this, "You are login", Toast.LENGTH_LONG).show();
 
-
+        Intent intent = new Intent(ActivityAccount.this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
     private void getUserProfile(AccessToken currentAccessToken) {
         GraphRequest request = GraphRequest.newMeRequest(
                 currentAccessToken, new GraphRequest.GraphJSONObjectCallback() {
